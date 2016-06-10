e478816942fc58d1541336807ad9cef8376afb4c
sun <slowburgy@gmail.com>
2016-06-10 21:23:22 +0900

    Add function FB OAUTH

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
-
+var session = require('express-session');
+var passport = require('passport')
+, FacebookStrategy = require('passport-facebook').Strategy;
+var util = require('util');
 var api = require('./api');
 
 var connection = mysql.createConnection({
@@ -28,9 +31,44 @@ var connection = mysql.createConnection({
     database: 'mysql'
 });
 
+// FACE BOOK OAUTH
+// serialize
+// 인증후 사용자 정보를 세션에 저장
+passport.serializeUser(function(user, done) {
+	console.log('serialize');
+	console.log("user : " + JSON.stringify(user));
+	done(null, user);
+});
+
+
+// deserialize
+// 인증후, 사용자 정보를 세션에서 읽어서 request.user에 저장
+passport.deserializeUser(function(user, done) {
+	//findById(id, function (err, user) {
+    console.log('deserialize');
+    done(null, user);
+    //});
+	});
+
+passport.use(new FacebookStrategy({
+    clientID: '1404570266509220',
+    clientSecret: '691a373e94e46fc1444baf8d03635846',
+    callbackURL: "http://143.248.48.156:3000/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    console.log(accessToken);
+    done(null,profile);
+}));
+
 app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// For FB OAUTH Session.
+app.use(session({ secret: 'your secret here' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
@@ -59,6 +97,33 @@ app.post('/api/review', api.review);
 app.get('/api/numpref', api.numpref);
 app.get('/api/getlike', api.getlike);
 app.get('/api/getreview', api.getreview);
+
+// FACEBOOK OAUTH 
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/login_success', failureRedirect: '/login_fail' }));
+app.get('/login_success', ensureAuthenticated, function(req, res){
+    res.send(req.user);
+    /*
+	Something will be added.
+    */
+});
+
+app.get('/logout', function(req, res){
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+    // if log in success, go to the next step.
+    if (req.isAuthenticated()) {
+        console.log("Login");
+        console.log("user :" + util.inspect(req.session.passport ));
+        return next(); }
+    // if not logged in, go to the login page.
+    console.log("Not logged in");
+    res.redirect('/');
+}
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
